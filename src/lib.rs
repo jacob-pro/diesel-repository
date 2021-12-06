a2ea0e6399f006c264edb0e5be17c7523fd8200b
Jacob Halsey <jacob@jhalsey.com>
2021-12-06 18:24:47 +0000

    new approach

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,23 @@
 use diesel::associations::HasTable;
-use diesel::helper_types::{Find, Limit};
 use diesel::prelude::*;
-use diesel::query_builder::{
-    AsChangeset, DeleteStatement, InsertStatement, IntoUpdateTarget, QueryFragment,
-};
-use diesel::query_dsl::limit_dsl::LimitDsl;
-use diesel::query_dsl::methods::{ExecuteDsl, FindDsl};
+use diesel::query_builder::InsertStatement;
 use diesel::query_dsl::LoadQuery;
 use diesel::Connection;
 
 type Tab<E> = <E as HasTable>::Table;
-type Backend<T> = <T as Connection>::Backend;
-type DeleteFindStatement<F> =
-    DeleteStatement<<F as HasTable>::Table, <F as IntoUpdateTarget>::WhereClause>;
 
-pub trait AbstractRepository<Entity, ID>
-where
-    Self::Conn: Connection,
-{
-    type Conn;
-
-    fn connection(&self) -> &Self::Conn;
-}
-
-pub trait CrudRepository<'t, Entity: 't, ID>: AbstractRepository<Entity, ID>
+pub trait CrudRepository<Entity, Id>
 where
     Entity: HasTable,
-    Tab<Entity>: FindDsl<ID>,
 {
-    fn delete(&self, entity: Entity) -> QueryResult<usize>
-    where
-        ID: Clone,
-        for<'a> &'a Entity: Identifiable<Id = &'a ID>,
-        Find<Tab<Entity>, ID>: IntoUpdateTarget,
-        DeleteFindStatement<Find<Tab<Entity>, ID>>: ExecuteDsl<Self::Conn>,
-    {
-        diesel::delete(QueryDsl::find(Entity::table(), entity.id().clone()))
-            .execute(self.connection())
-    }
+    type Conn: Connection;
 
-    fn delete_by_id(&self, id: ID) -> QueryResult<usize>
-    where
-        Find<Tab<Entity>, ID>: IntoUpdateTarget,
-        DeleteFindStatement<Find<Tab<Entity>, ID>>: ExecuteDsl<Self::Conn>,
-    {
-        diesel::delete(QueryDsl::find(Entity::table(), id)).execute(self.connection())
-    }
-
-    fn find_by_id(&self, id: ID) -> QueryResult<Option<Entity>>
-    where
-        Find<Tab<Entity>, ID>: LimitDsl + Table,
-        Limit<Find<Tab<Entity>, ID>>: LoadQuery<Self::Conn, Entity>,
-    {
-        diesel::QueryDsl::find(Entity::table(), id)
-            .first::<Entity>(self.connection())
-            .optional()
-    }
-
-    fn find_all(&self) -> QueryResult<Vec<Entity>>
-    where
-        Tab<Entity>: LoadQuery<Self::Conn, Entity>,
-    {
-        Entity::table().load(self.connection())
-    }
+    fn connection(&self) -> &Self::Conn;
+    fn delete(&self, entity: Entity) -> QueryResult<usize>;
+    fn delete_by_id(&self, id: Id) -> QueryResult<usize>;
+    fn find_by_id(&self, id: Id) -> QueryResult<Option<Entity>>;
+    fn find_all(&self) -> QueryResult<Vec<Entity>>;
+    fn update(&self, entity: &Entity) -> QueryResult<usize>;
 
     fn insert<N>(&self, new_entity: N) -> QueryResult<Entity>
     where
@@ -74,20 +29,49 @@ where
             .values(new_entity)
             .get_result(self.connection())
     }
+}
 
-    fn update<V>(&self, entity: &'t Entity) -> QueryResult<usize>
-    where
-        Self::Conn: Connection,
-        &'t Entity: Identifiable<Table = Tab<Entity>> + AsChangeset<Target = Tab<Entity>>,
-        Tab<Entity>: FindDsl<<&'t Entity as Identifiable>::Id>,
-        Find<Tab<Entity>, <&'t Entity as Identifiable>::Id>:
-            IntoUpdateTarget<Table = Tab<Entity>, WhereClause = V>,
-        V: QueryFragment<Backend<Self::Conn>>,
-        <&'t Entity as AsChangeset>::Changeset: QueryFragment<Backend<Self::Conn>>,
-        <Tab<Entity> as QuerySource>::FromClause: QueryFragment<Backend<Self::Conn>>,
-    {
-        diesel::update(entity)
-            .set(entity)
-            .execute(self.connection())
-    }
+#[macro_export]
+macro_rules! implement_crud_repository {
+    ( $name:ident, $entity:ty, $key:ty, $conn:ty ) => {
+        pub struct $name<'l>(&'l $conn);
+
+        impl<'l> $name<'l> {
+            pub fn new(connection: &'l $conn) -> Self {
+                Self(connection)
+            }
+        }
+
+        impl diesel_repository::CrudRepository<$entity, $key> for $name<'_> {
+            type Conn = $conn;
+
+            fn connection(&self) -> &Self::Conn {
+                &self.0
+            }
+            fn delete(&self, entity: $entity) -> QueryResult<usize> {
+                use diesel::associations::HasTable;
+                diesel::delete(<$entity>::table().find(entity.id())).execute(self.connection())
+            }
+            fn delete_by_id(&self, id: $key) -> QueryResult<usize> {
+                use diesel::associations::HasTable;
+                diesel::delete(<$entity>::table().find(id)).execute(self.connection())
+            }
+            fn find_by_id(&self, id: $key) -> QueryResult<Option<$entity>> {
+                use diesel::associations::HasTable;
+                <$entity>::table()
+                    .find(id)
+                    .first::<$entity>(self.connection())
+                    .optional()
+            }
+            fn find_all(&self) -> QueryResult<Vec<$entity>> {
+                use diesel::associations::HasTable;
+                <$entity>::table().load(self.connection())
+            }
+            fn update(&self, entity: &$entity) -> QueryResult<usize> {
+                diesel::update(entity)
+                    .set(entity)
+                    .execute(self.connection())
+            }
+        }
+    };
 }

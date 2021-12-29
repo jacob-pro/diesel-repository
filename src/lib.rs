4f9e0ab72ba906bde472fd0167986a9dfe141e89
Jacob Halsey <jacob@jhalsey.com>
2021-12-29 20:54:03 +0000

    add count()

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,12 @@ where
     type Conn: Connection;
 
     fn connection(&self) -> &Self::Conn;
-    fn delete(&self, entity: Entity) -> QueryResult<usize>;
-    fn delete_by_id(&self, id: Id) -> QueryResult<usize>;
+    fn delete(&self, entity: Entity) -> QueryResult<bool>;
+    fn delete_by_id(&self, id: Id) -> QueryResult<bool>;
     fn find_by_id(&self, id: Id) -> QueryResult<Option<Entity>>;
     fn find_all(&self) -> QueryResult<Vec<Entity>>;
     fn update(&self, entity: &Entity) -> QueryResult<usize>;
+    fn count(&self) -> QueryResult<u64>;
 
     fn insert<N>(&self, new_entity: N) -> QueryResult<Entity>
     where
@@ -48,13 +49,23 @@ macro_rules! implement_crud_repository {
             fn connection(&self) -> &Self::Conn {
                 &self.0
             }
-            fn delete(&self, entity: $entity) -> QueryResult<usize> {
+            fn delete(&self, entity: $entity) -> QueryResult<bool> {
                 use diesel::associations::HasTable;
-                diesel::delete(<$entity>::table().find(entity.id())).execute(self.connection())
+                diesel::delete(<$entity>::table().find(entity.id()))
+                    .execute(self.connection())
+                    .map(|affected| {
+                        assert!(affected <= 1);
+                        affected > 0
+                    })
             }
-            fn delete_by_id(&self, id: $key) -> QueryResult<usize> {
+            fn delete_by_id(&self, id: $key) -> QueryResult<bool> {
                 use diesel::associations::HasTable;
-                diesel::delete(<$entity>::table().find(id)).execute(self.connection())
+                diesel::delete(<$entity>::table().find(id))
+                    .execute(self.connection())
+                    .map(|affected| {
+                        assert!(affected <= 1);
+                        affected > 0
+                    })
             }
             fn find_by_id(&self, id: $key) -> QueryResult<Option<$entity>> {
                 use diesel::associations::HasTable;
@@ -71,6 +82,14 @@ macro_rules! implement_crud_repository {
                 diesel::update(entity)
                     .set(entity)
                     .execute(self.connection())
+            }
+            fn count(&self) -> QueryResult<u64> {
+                use diesel::associations::HasTable;
+                use std::convert::TryFrom;
+                <$entity>::table()
+                    .count()
+                    .first(self.connection())
+                    .map(|x: i64| u64::try_from(x).unwrap())
             }
         }
     };
